9104358d5aba1a94390ef2e1a3e8779bb54b7828
Wadii-Sdouga <64696398+Wadii-Sdouga@users.noreply.github.com>
2021-10-22 22:44:09 +0100

    Update StringManipulator.java

--- src/tp1/StringManipulator.java
+++ src/tp1/StringManipulator.java
@@ -9,28 +9,59 @@ public class StringManipulator {
     }
 
     public void reverse(){
-        //do your stuff
-        System.out.println("reverse : ");
+        String ch="";
+        for(int i = s.length()-1; i>=0; i--){
+            ch = ch + s.charAt(i);
+    }
+        System.out.println("reverse : "+ch);
     }
 
     public void isPalindrome(){
-        //do your stuff
-        System.out.println("isPalindrome : ");
+        int j;
+        boolean test;
+        test=true;
+        j=s.length()-1;
+        for(int i = 0; i<(s.length()/2)-1; i++)
+            if (s.charAt(i)!=s.charAt(j))
+                test=false;
+                
+                
+        if (test==true)
+        System.out.println(ch+"palindrome");
+        else
+         System.out.println(ch+"is not palindrome");
+
     }
 
     public void toUpperCase(){
-        //do your stuff
-        System.out.println("uppercase : ");
+       String s1upper=s.toUpperCase();  
+
+        System.out.println("uppercase : "+s1upper);
     }
 
     public void toLowerCase(){
-        //do your stuff
-        System.out.println("lowercase : ");
+         String s1upper=s.toLowerCase();  
+
+
+        System.out.println("lowercase : "+s1upper);
     }
 
     public void getVowelNumber(){
-        //do your stuff
-        System.out.println("vowelNumber : ");
+        char ch;
+	int i=0;
+	for(int j=0;j<s.length();j++)
+	{
+	  ch=s.charAt(j);	
+	  if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+	  {
+            i++;
+	 
+	  }
+	}
+	if(i==0)
+		System.out.println("There are no vowels in a string"); 
+	else 	
+        System.out.println("vowelNumber : "+i);
     }
 
     public static void main(String[] args) {
